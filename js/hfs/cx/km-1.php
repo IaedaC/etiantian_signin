367db419503f5e6d9a76e8c08576a7359eacb1f1
Jiahang Yang <BakaMihu@mcraft.cn>
2020-04-16 16:12:40 +0800

    delete hfs dir

--- js/hfs/cx/km-1.php
+++
@@ -1,9 +0,0 @@
-<meta charset="utf-8">  
-<?php 
-$url = "http://62.234.20.151/cx/km-2.php?examid={$_GET["examid"]}";
-$contents = file_get_contents($url);
-echo file_put_contents("km.json",$contents);
-$content = file_get_contents('km.json');
-$content = str_replace('</br>','',$content);
-file_put_contents('km.json',$content);
-?>
